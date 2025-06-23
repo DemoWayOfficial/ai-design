1ab81d25e7fcf84f1afdd93d7e5879ead5904f24
intellild <intellild@outlook.com>
2025-06-23 10:27:53 +0800

    init shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   future: {
@@ -5,8 +7,9 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  devServer: {
-    host: '127.0.0.1',
+  css: ['~/assets/css/tailwind.css'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
