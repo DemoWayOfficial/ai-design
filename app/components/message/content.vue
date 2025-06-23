5b8f84a8e3be604f234c3f4308450dd017d2433e
intellild <intellild@outlook.com>
2025-06-23 18:11:20 +0800

    markdown docontent

--- app/components/message/content.vue
+++ app/components/message/content.vue
@@ -1,45 +1,51 @@
 <template>
   <template v-if="token.type === 'space'" />
-  <Primitive v-else :as="content.tag">
-    <template v-if="content.children">
-      {{ content.children }}
+  <br v-else-if="token.type === 'br'" />
+  <p v-else-if="token.type === 'p'">
+    <template v-if="token.tokens">
+      <MessageContent
+        v-for="(child, index) of token.tokens"
+        :key="index"
+        v-memo="child.raw"
+        :token="child"
+      />
     </template>
-  </Primitive>
+    <template v-else>
+      {{ token.raw }}
+    </template>
+  </p>
+  <component :is="`h${token.depth}`" v-else-if="token.type === 'heading'">
+    <MessageContent
+      v-for="(child, index) of token.tokens"
+      :key="index"
+      v-memo="child.raw"
+      :token="child"
+    />
+  </component>
+  <template v-else-if="token.type === 'text'">
+    <template v-if="token.tokens">
+      <MessageContent
+        v-for="(child, index) of token.tokens"
+        :key="index"
+        v-memo="child.raw"
+        :token="child"
+      />
+    </template>
+    <template v-else>
+      {{ token.raw }}
+    </template>
+  </template>
+  <div v-else>
+    {{ token.raw }}
+  </div>
 </template>
 
 <script setup lang="ts">
-import type { Token } from 'marked';
-import { Primitive } from 'reka-ui';
+// https://github.com/markedjs/marked/blob/master/src/Renderer.ts
 
-const props = defineProps<{
+import type { Token } from 'marked';
+
+defineProps<{
   token: Token;
 }>();
-
-// https://github.com/markedjs/marked/blob/master/src/Renderer.ts
-const content = computed(() => {
-  const { token } = props;
-
-  switch (token.type) {
-    case 'heading':
-      return {
-        tag: `h${token.depth}`,
-        children: token.text,
-      };
-    case 'br':
-      return {
-        tag: 'br',
-      };
-    case 'paragraph':
-      return {
-        tag: 'p',
-        children: token.text,
-      };
-    default: {
-      return {
-        tag: 'div',
-        children: token.raw,
-      };
-    }
-  }
-});
 </script>
