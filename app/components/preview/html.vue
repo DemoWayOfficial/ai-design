5b0b5bd0e74e3057623caa00888eb047387b5f95
intellild <intellild@outlook.com>
2025-06-24 17:53:10 +0800

    use srcdoc instead of document.write (#2)

--- app/components/preview/html.vue
+++ app/components/preview/html.vue
@@ -1,31 +1,9 @@
 <template>
-  <iframe ref="iframeRef" sandbox="allow-same-origin allow-scripts" />
+  <iframe sandbox="allow-same-origin allow-scripts" :srcdoc="code" />
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
+defineProps<{
   code: string;
 }>();
-
-const iframeRef = ref<HTMLIFrameElement>();
-
-onMounted(() => {
-  watch(
-    () => props.code,
-    (code) => {
-      const iframe = iframeRef.value;
-      if (!iframe?.contentDocument) {
-        return;
-      }
-      const doc = iframe.contentDocument;
-      doc.open('text/html', 'replace');
-      doc.writeln('<!DOCTYPE html>');
-      doc.writeln(code);
-      doc.close();
-    },
-    {
-      immediate: true,
-    },
-  );
-});
 </script>
