4980d937f0666e2d694e34aa1e6e6de29ed5f99d
intellild <intellild@outlook.com>
2025-06-27 12:43:49 +0800

    tabs

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex h-screen w-screen flex-col overflow-hidden">
     <div
-      class="flex h-[60px] w-full flex-none items-center overflow-hidden border-b p-4"
+      class="flex h-[50px] w-full flex-none items-center overflow-hidden border-b p-4"
     >
       <SessionsRoot />
     </div>
 
-    <div class="w-full flex-1">
+    <div class="w-full flex-1 overflow-hidden">
       <NuxtPage />
     </div>
   </div>
--- app/components/chat/root.vue
+++ app/components/chat/root.vue
@@ -1,28 +1,31 @@
 <template>
   <div :class="cn('bg-muted flex gap-4 p-4', props.class)">
-    <ui-tabs
-      v-model="activeTab"
-      class="bg-background flex-1 overflow-hidden rounded-xl border shadow"
+    <div
+      class="bg-background flex flex-1 flex-col overflow-hidden rounded-xl border shadow"
     >
-      <ui-tabs-list class="border-t">
-        <ui-tabs-trigger
+      <div
+        class="flex h-[40px] w-full flex-none items-center gap-1 border-b px-1"
+      >
+        <ui-button
           v-for="preview of previews"
           :key="preview.id"
-          :value="preview.id"
+          :variant="active === preview.id ? 'default' : 'secondary'"
+          size="sm"
+          class="h-7"
+          @click="active = preview.id"
         >
           {{ `${preview.id}.${preview.lang}` }}
-        </ui-tabs-trigger>
-      </ui-tabs-list>
+        </ui-button>
+      </div>
 
-      <ui-tabs-content
-        v-for="preview of previews"
-        :key="preview.id"
-        :value="preview.id"
-      >
-        <PreviewRoot class="size-full" :preview="preview">preview</PreviewRoot>
-      </ui-tabs-content>
-    </ui-tabs>
-
+      <div class="flex-1">
+        <PreviewRoot
+          v-if="activePreview"
+          class="size-full"
+          :preview="activePreview"
+        />
+      </div>
+    </div>
     <div class="flex w-md flex-none flex-col overflow-hidden">
       <chat-messages class="flex-1" :messages="messages" />
       <chat-input
@@ -52,7 +55,14 @@ const props = defineProps<{
 
 const previews = ref<Preview[]>([]);
 
-const activeTab = ref<string>();
+const active = ref<string>();
+
+const activePreview = computed(() => {
+  if (!active.value) {
+    return undefined;
+  }
+  return previews.value.find((item) => item.id === active.value);
+});
 
 const { messages, input, handleSubmit } = useChat({
   model: () => props.model,
@@ -68,6 +78,10 @@ const { messages, input, handleSubmit } = useChat({
           value: token.text,
         });
       }
+    }
+
+    if (!active.value) {
+      active.value = previews.value.at(-1)?.id;
     }
   },
   onRestore(messages) {
@@ -89,8 +103,8 @@ const { messages, input, handleSubmit } = useChat({
       }
     }
 
-    if (!activeTab.value) {
-      activeTab.value = previews.value.at(-1)?.id;
+    if (!active.value) {
+      active.value = previews.value.at(-1)?.id;
     }
   },
 });
